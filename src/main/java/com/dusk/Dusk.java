fcb3c6f185a2064b691721205929a15c721a534a
someaddons <someaddons@github.com>
2021-05-02 00:55:55 +0200

    intial

---
+++ src/main/java/com/dusk/Dusk.java
@@ -0,0 +1,41 @@
+package com.dusk;
+
+import com.dusk.config.Configuration;
+import com.dusk.event.EventHandler;
+import com.dusk.event.ModEventHandler;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import static com.dusk.Dusk.MODID;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(MODID)
+public class Dusk
+{
+    public static final String MODID = "dusk";
+
+    private static final Logger        LOGGER = LogManager.getLogger();
+    public static        Configuration config;
+
+    public Dusk()
+    {
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+
+        config = new Configuration();
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        Mod.EventBusSubscriber.Bus.MOD.bus().get().register(ModEventHandler.class);
+        Mod.EventBusSubscriber.Bus.FORGE.bus().get().register(EventHandler.class);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        LOGGER.info("Dusk initialized");
+    }
+}
---
+++ src/main/java/com/dusk/config/CommonConfiguration.java
@@ -0,0 +1,26 @@
+package com.dusk.config;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+
+public class CommonConfiguration
+{
+    public final ForgeConfigSpec.ConfigValue<Integer> nightSpawnMod;
+    public final ForgeConfigSpec.ConfigValue<Integer> minSleepTime;
+    public final ForgeConfigSpec.ConfigValue<Boolean> disableSleep;
+
+    protected CommonConfiguration(final ForgeConfigSpec.Builder builder)
+    {
+        builder.push("Dusk settings");
+        builder.comment("Percentage of how many more monster can appear at night, default: 30");
+        nightSpawnMod = builder.defineInRange("nightSpawnMod", 30, 1, 5000);
+
+        builder.comment("Min time required to sleep, vanilla min time:12500, Midnight is 18000");
+        minSleepTime = builder.defineInRange("minSleepTime", 13500, 1, 23000);
+
+        builder.comment("Disable sleeping?, default: false");
+        disableSleep = builder.define("disableSleep", false);
+
+        // Escapes the current category level
+        builder.pop();
+    }
+}
