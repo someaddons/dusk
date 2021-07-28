a8ef70569ad1f32a1540fbc20b1484fa8779b218
Someaddon <someaddon@github.com>
2021-07-28 19:30:51 +0200

    1.17 port

--- src/main/java/com/dusk/Dusk.java
+++ src/main/java/com/dusk/Dusk.java
@@ -3,13 +3,12 @@ package com.dusk;
 import com.dusk.config.Configuration;
 import com.dusk.event.EventHandler;
 import com.dusk.event.ModEventHandler;
-import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fml.network.FMLNetworkConstants;
-import org.apache.commons.lang3.tuple.Pair;
+import net.minecraftforge.fmllegacy.network.FMLNetworkConstants;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -17,16 +16,15 @@ import static com.dusk.Dusk.MODID;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(MODID)
-public class Dusk
-{
+public class Dusk {
     public static final String MODID = "dusk";
 
-    private static final Logger        LOGGER = LogManager.getLogger();
-    public static        Configuration config;
+    private static final Logger LOGGER = LogManager.getLogger();
+    public static Configuration config;
 
-    public Dusk()
-    {
-        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+    public Dusk() {
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class,
+                () -> new IExtensionPoint.DisplayTest(() -> FMLNetworkConstants.IGNORESERVERONLY, (remote, isServer) -> true));
 
         config = new Configuration();
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
@@ -34,8 +32,7 @@ public class Dusk
         Mod.EventBusSubscriber.Bus.FORGE.bus().get().register(EventHandler.class);
     }
 
-    private void setup(final FMLCommonSetupEvent event)
-    {
+    private void setup(final FMLCommonSetupEvent event) {
         LOGGER.info("Dusk initialized");
     }
 }
--- src/main/java/com/dusk/event/EventHandler.java
+++ src/main/java/com/dusk/event/EventHandler.java
@@ -1,10 +1,10 @@
 package com.dusk.event;
 
 import com.dusk.Dusk;
-import net.minecraft.entity.EntityClassification;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.world.World;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.player.PlayerSleepInBedEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -12,26 +12,21 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 /**
  * Handler to catch server tick events
  */
-public class EventHandler
-{
-    private static int    tickCounter   = 0;
+public class EventHandler {
+    private static int tickCounter = 0;
     private static double spawnModifier = 1.0d;
 
     @SubscribeEvent
-    public static void onWorldTick(final TickEvent.WorldTickEvent event)
-    {
-        if (event.world.isClientSide() || event.phase == TickEvent.Phase.START)
-        {
+    public static void onWorldTick(final TickEvent.WorldTickEvent event) {
+        if (event.world.isClientSide() || event.phase == TickEvent.Phase.START) {
             return;
         }
 
-        if ((event.world.dimension() != World.OVERWORLD))
-        {
+        if ((event.world.dimension() != Level.OVERWORLD)) {
             return;
         }
 
-        if (++tickCounter != 20)
-        {
+        if (++tickCounter != 20) {
             return;
         }
         tickCounter = 0;
@@ -42,30 +37,25 @@ public class EventHandler
     /**
      * Change vanilla values
      */
-    private static void adjustClassification()
-    {
-        EntityClassification.MONSTER.max = (int) (Dusk.config.getCommonConfig().baseMonsterCap.get() * spawnModifier);
+    private static void adjustClassification() {
+        MobCategory.MONSTER.max = (int) (Dusk.config.getCommonConfig().baseMonsterCap.get() * spawnModifier);
     }
 
     /**
      * Linear raise of modifier
      */
     private static final int RAISE_START = 12000;
-    private static final int RAISE_STOP  = 16000;
-    private static final int DROP_START  = 20000;
+    private static final int RAISE_STOP = 16000;
+    private static final int DROP_START = 20000;
 
     private static final double INTERVAL = ((RAISE_STOP - RAISE_START) / 20.0d) * 100.0d;
 
     // 1800 Midnight 24000(0) sunrise 13000 night 12500 sleep time -> Raise 12000 -> 16000, drop >20000 -> <12000
-    private static void adjustSpawnModifier(final long time)
-    {
-        if (time <= RAISE_STOP && time >= RAISE_START)
-        {
+    private static void adjustSpawnModifier(final long time) {
+        if (time <= RAISE_STOP && time >= RAISE_START) {
             spawnModifier += Dusk.config.getCommonConfig().nightSpawnMod.get() / INTERVAL;
             adjustClassification();
-        }
-        else if (spawnModifier > 1.0d && (time > DROP_START || time < RAISE_START))
-        {
+        } else if (spawnModifier > 1.0d && (time > DROP_START || time < RAISE_START)) {
             spawnModifier -= Dusk.config.getCommonConfig().nightSpawnMod.get() / INTERVAL;
             spawnModifier = Math.max(spawnModifier, 1.0d);
             adjustClassification();
@@ -73,18 +63,15 @@ public class EventHandler
     }
 
     @SubscribeEvent
-    public static void onPlayerSleep(PlayerSleepInBedEvent event)
-    {
-        if (Dusk.config.getCommonConfig().disableSleep.get() || event.getPlayer().level.getDayTime() < Dusk.config.getCommonConfig().minSleepTime.get())
-        {
-            event.setResult(PlayerEntity.SleepResult.NOT_POSSIBLE_NOW);
+    public static void onPlayerSleep(PlayerSleepInBedEvent event) {
+        if (Dusk.config.getCommonConfig().disableSleep.get() || event.getPlayer().level.getDayTime() < Dusk.config.getCommonConfig().minSleepTime.get()) {
+            event.setResult(Player.BedSleepingProblem.NOT_POSSIBLE_NOW);
         }
 
-        if (event.getPlayer().level.isClientSide())
-        {
+        if (event.getPlayer().level.isClientSide()) {
             return;
         }
 
-        ((ServerPlayerEntity) event.getPlayer()).setRespawnPosition(event.getPlayer().level.dimension(), event.getPos(), event.getPlayer().yRot, false, true);
+        ((ServerPlayer) event.getPlayer()).setRespawnPosition(event.getPlayer().level.dimension(), event.getPos(), event.getPlayer().getYRot(), false, true);
     }
 }
