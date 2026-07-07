7362aa05fb3eb38140875266cfda10afb31398d7
someaddons <someaddons@github.com>
2026-07-07 23:21:12 +0200

    4.1: Add new feature allowing mobs to receive a random potions when combat starts during night

--- src/main/java/com/dusk/config/CommonConfiguration.java
+++ src/main/java/com/dusk/config/CommonConfiguration.java
@@ -1,22 +1,38 @@
 package com.dusk.config;
 
 import com.cupboard.config.ICommonConfig;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
 
 public class CommonConfiguration implements ICommonConfig
 {
-    public int     nightSpawnMod          = 30;
-    public int     sleepDisableStartTime  = 10000;
-    public int     sleepDisableEndTime    = 14000;
-    public boolean enableSleepRestriction = true;
-    public int     baseMonsterCap         = 70;
-    public boolean disableSleep           = false;
-    public int nightSpawnMaxBlockLight = 1;
-    public double nightAwarenessRangeMultiplier = 1.2;
+    public int     nightSpawnMod                 = 30;
+    public int     sleepDisableStartTime         = 10000;
+    public int     sleepDisableEndTime           = 14000;
+    public boolean enableSleepRestriction        = true;
+    public int     baseMonsterCap                = 70;
+    public boolean disableSleep                  = false;
+    public int     nightSpawnMaxBlockLight       = 1;
+    public double  nightAwarenessRangeMultiplier = 1.2;
+    public boolean enableNightBlessings          = true;
+    public int     blessingChancePercent         = 20;
+
+    private final Random            random          = new Random();
+    public        List<PotionEntry> potionEntryList = new ArrayList<>();
 
     public CommonConfiguration()
     {
-
+        addPotionEntry(new PotionEntry(ResourceLocation.tryParse("minecraft:regeneration"), 1, 30, 5));
+        addPotionEntry(new PotionEntry(ResourceLocation.tryParse("minecraft:resistance"), 1, 30, 5));
+        addPotionEntry(new PotionEntry(ResourceLocation.tryParse("minecraft:absorption"), 1, 30, 5));
+        addPotionEntry(new PotionEntry(ResourceLocation.tryParse("minecraft:glowing"), 1, 30, 5));
     }
 
     @Override
@@ -54,6 +70,30 @@ public class CommonConfiguration implements ICommonConfig
         entry6.addProperty("nightAwarenessRangeMultiplier", nightAwarenessRangeMultiplier);
         root.add("nightAwarenessRangeMultiplier", entry6);
 
+        final JsonObject entry7 = new JsonObject();
+        entry7.addProperty("desc:", "Enables mobs to receive a potion blessing at the start of combat during the night.");
+        entry7.addProperty("enabled", enableNightBlessings);
+        entry7.addProperty("blessingChancePercent", blessingChancePercent);
+
+        final JsonArray potions = new JsonArray();
+        final HashSet<ResourceLocation> used = new HashSet<>();
+        for (final PotionEntry potionEntry : potionEntryList)
+        {
+            if (!used.contains(potionEntry.potionID))
+            {
+                used.add(potionEntry.potionID);
+                final JsonObject singlePotion = new JsonObject();
+                singlePotion.addProperty("potionID", potionEntry.potionID.toString());
+                singlePotion.addProperty("level", potionEntry.level);
+                singlePotion.addProperty("secondsDuration", potionEntry.secondsDuration);
+                singlePotion.addProperty("weight", potionEntry.weight);
+                potions.add(singlePotion);
+            }
+        }
+        entry7.add("potions", potions);
+
+        root.add("nightBlessings", entry7);
+
         final JsonObject entry2 = new JsonObject();
         entry2.addProperty("desc:",
             "Base monster cap per player the nightSpawnMod modifier increases this until midnight after which it decreases again until morning. Increase this to see more monsters in the world or to match other mods changing the base, default(Vanilla): 70");
@@ -74,5 +114,36 @@ public class CommonConfiguration implements ICommonConfig
         disableSleep = data.get("disableSleep").getAsJsonObject().get("disableSleep").getAsBoolean();
         nightSpawnMaxBlockLight = data.get("nightSpawnMaxBlockLight").getAsJsonObject().get("nightSpawnMaxBlockLight").getAsInt();
         nightAwarenessRangeMultiplier = data.get("nightAwarenessRangeMultiplier").getAsJsonObject().get("nightAwarenessRangeMultiplier").getAsDouble();
+
+        blessingChancePercent = data.get("nightBlessings").getAsJsonObject().get("blessingChancePercent").getAsInt();
+        enableNightBlessings = data.get("nightBlessings").getAsJsonObject().get("enabled").getAsBoolean();
+
+        JsonArray potions = data.get("nightBlessings").getAsJsonObject().get("potions").getAsJsonArray();
+        for (final JsonElement jsonElement : potions)
+        {
+            final ResourceLocation potionID = ResourceLocation.tryParse(jsonElement.getAsJsonObject().get("potionID").getAsString());
+            final int level = Math.max(1, jsonElement.getAsJsonObject().get("level").getAsInt());
+            final int secondsDuration = jsonElement.getAsJsonObject().get("secondsDuration").getAsInt();
+            final int weight = Math.max(1, jsonElement.getAsJsonObject().get("weight").getAsInt());
+            addPotionEntry(new PotionEntry(potionID, level, secondsDuration, weight));
+        }
+    }
+
+    private void addPotionEntry(final PotionEntry regeneration)
+    {
+        for (int i = 0; i < regeneration.weight; i++)
+        {
+            potionEntryList.add(regeneration);
+        }
+    }
+
+    public PotionEntry getRandomPotionEntry()
+    {
+        if (potionEntryList.isEmpty() || random.nextInt(100) >= blessingChancePercent)
+        {
+            return null;
+        }
+
+        return potionEntryList.get(random.nextInt(potionEntryList.size()));
     }
 }
