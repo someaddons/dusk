3c18542e24ac75c1e23a77d6920692b5ce3e46e2
someaddons <someaddons@github.com>
2026-07-08 10:30:14 +0200

    3.8: Reworked sleep time changes, you can now configure a time range in which sleeping is disallowed or disable the feature completly allowing for better compat with other mods changing sleeping

--- src/main/java/com/dusk/config/CommonConfiguration.java
+++ src/main/java/com/dusk/config/CommonConfiguration.java
@@ -1,19 +1,41 @@
 package com.dusk.config;
 
-import com.dusk.Dusk;
 import com.cupboard.config.ICommonConfig;
 import com.google.gson.JsonObject;
 
 public class CommonConfiguration implements ICommonConfig
 {
-    public int     nightSpawnMod  = 30;
-    public int     minSleepTime   = 14500;
-    public int     baseMonsterCap = 70;
-    public boolean disableSleep   = false;
+    public int     nightSpawnMod          = 30;
+    public int     sleepDisableStartTime  = 10000;
+    public int     sleepDisableEndTime    = 14000;
+    public boolean enableSleepRestriction = true;
+    public int     baseMonsterCap         = 70;
+    public boolean disableSleep           = false;
 
+    public CommonConfiguration()
+    {
+
+    }
+
+    @Override
     public JsonObject serialize()
     {
         final JsonObject root = new JsonObject();
+
+        final JsonObject entry3 = new JsonObject();
+        entry3.addProperty("desc:",
+            "Sets a time-window in which sleeping is disabled. Normal vanilla sleep time start at 12500. By default this mod delays it to " + sleepDisableEndTime
+                + " which allows some mobs to spawn."
+                + " To understand minecraft time values check: https://minecraft.wiki/w/Daylight_cycle#Daytime");
+        entry3.addProperty("enabled", enableSleepRestriction);
+        entry3.addProperty("sleepDisableStartTime", sleepDisableStartTime);
+        entry3.addProperty("sleepDisableEndTime", sleepDisableEndTime);
+        root.add("sleepTime", entry3);
+
+        final JsonObject entry4 = new JsonObject();
+        entry4.addProperty("desc:", "Disable sleeping?, default: false");
+        entry4.addProperty("disableSleep", disableSleep);
+        root.add("disableSleep", entry4);
 
         final JsonObject entry = new JsonObject();
         entry.addProperty("desc:", "Percentage of how many more monster can appear at night, default: 30");
@@ -22,41 +44,21 @@ public class CommonConfiguration implements ICommonConfig
 
         final JsonObject entry2 = new JsonObject();
         entry2.addProperty("desc:",
-          "Base monster cap to act upon, this is the value it'll increase from/reduce to at the start/end of the night. Increase this to see more monsters in the world, default(Vanilla): 70");
+            "Base monster cap per player the nightSpawnMod modifier increases this until midnight after which it decreases again until morning. Increase this to see more monsters in the world or to match other mods changing the base, default(Vanilla): 70");
         entry2.addProperty("baseMonsterCap", baseMonsterCap);
         root.add("baseMonsterCap", entry2);
-
-        final JsonObject entry3 = new JsonObject();
-        entry3.addProperty("desc:", "Min time required to sleep, default = 14500, vanilla default time:12500, Midnight is 18000. Max: 23000");
-        entry3.addProperty("minSleepTime", minSleepTime);
-        root.add("minSleepTime", entry3);
-
-        final JsonObject entry4 = new JsonObject();
-        entry4.addProperty("desc:", "Disables sleeping entirely, default: false");
-        entry4.addProperty("disableSleep", disableSleep);
-        root.add("disableSleep", entry4);
 
         return root;
     }
 
-    public void deserialize(JsonObject data)
+    @Override
+    public void deserialize(final JsonObject data)
     {
-        if (data == null)
-        {
-            Dusk.LOGGER.error("Config file was empty!");
-            return;
-        }
-
-        try
-        {
-            nightSpawnMod = data.get("nightSpawnMod").getAsJsonObject().get("nightSpawnMod").getAsInt();
-            baseMonsterCap = data.get("baseMonsterCap").getAsJsonObject().get("baseMonsterCap").getAsInt();
-            minSleepTime = data.get("minSleepTime").getAsJsonObject().get("minSleepTime").getAsInt();
-            disableSleep = data.get("disableSleep").getAsJsonObject().get("disableSleep").getAsBoolean();
-        }
-        catch (Exception e)
-        {
-            Dusk.LOGGER.error("Could not parse config file", e);
-        }
+        nightSpawnMod = data.get("nightSpawnMod").getAsJsonObject().get("nightSpawnMod").getAsInt();
+        baseMonsterCap = data.get("baseMonsterCap").getAsJsonObject().get("baseMonsterCap").getAsInt();
+        sleepDisableStartTime = data.get("sleepTime").getAsJsonObject().get("sleepDisableStartTime").getAsInt();
+        sleepDisableEndTime = data.get("sleepTime").getAsJsonObject().get("sleepDisableEndTime").getAsInt();
+        enableSleepRestriction = data.get("sleepTime").getAsJsonObject().get("enabled").getAsBoolean();
+        disableSleep = data.get("disableSleep").getAsJsonObject().get("disableSleep").getAsBoolean();
     }
 }
--- src/main/java/com/dusk/event/EventHandler.java
+++ src/main/java/com/dusk/event/EventHandler.java
@@ -75,11 +75,13 @@ public class EventHandler
     }
 
     public static void onPlayerSleep(
-      final ServerPlayer serverPlayerEntity,
-      final BlockPos pos,
-      final CallbackInfoReturnable<Either<Player.BedSleepingProblem, Unit>> cir)
+        final ServerPlayer serverPlayerEntity,
+        final BlockPos pos,
+        final CallbackInfoReturnable<Either<Player.BedSleepingProblem, Unit>> cir)
     {
-        if (Dusk.config.getCommonConfig().disableSleep || (serverPlayerEntity.level().getDayTime() % 24000) < Dusk.config.getCommonConfig().minSleepTime)
+        final long daytime = (serverPlayerEntity.level().getDayTime() % 24000);
+        if (Dusk.config.getCommonConfig().disableSleep || (Dusk.config.getCommonConfig().enableSleepRestriction && daytime < Dusk.config.getCommonConfig().sleepDisableEndTime
+            && daytime > Dusk.config.getCommonConfig().sleepDisableStartTime))
         {
             cir.setReturnValue(Either.left(Player.BedSleepingProblem.NOT_POSSIBLE_NOW));
         }
