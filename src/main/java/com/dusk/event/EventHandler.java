0a5d1a5c1915ec60da2f584da035605594e9e559
someaddons <someaddons@github.com>
2026-09-18 06:54:24 +0200

    26.3

--- src/main/java/com/dusk/event/EventHandler.java
+++ src/main/java/com/dusk/event/EventHandler.java
@@ -79,8 +79,8 @@ public class EventHandler
 
     public static void onPlayerSleep(
       final ServerPlayer serverPlayerEntity,
-      final BlockPos pos,
-      final CallbackInfoReturnable<Either<Player.BedSleepingProblem, Unit>> cir)
+        final CallbackInfoReturnable<Either<Player.BedSleepingProblem, Unit>> cir,
+        final BlockPos pos)
     {
         final long daytime = (serverPlayerEntity.level().getDefaultClockTime() % 24000);
         if (Dusk.config.getCommonConfig().disableSleep || (Dusk.config.getCommonConfig().enableSleepRestriction && daytime < Dusk.config.getCommonConfig().sleepDisableEndTime
@@ -91,14 +91,14 @@ public class EventHandler
         }
     }
 
-    public static void onPlayerSleep(final ServerPlayer serverPlayerEntity, final BlockPos pos, final CallbackInfo ci)
+    public static void onPlayerSleep(final ServerPlayer serverPlayerEntity, final BlockPos pos, final CallbackInfoReturnable<Boolean> cir)
     {
         final long daytime = (serverPlayerEntity.level().getDefaultClockTime() % 24000);
         if (Dusk.config.getCommonConfig().disableSleep || (Dusk.config.getCommonConfig().enableSleepRestriction && daytime < Dusk.config.getCommonConfig().sleepDisableEndTime
             && daytime > Dusk.config.getCommonConfig().sleepDisableStartTime))
         {
             serverPlayerEntity.sendSystemMessage(Component.translatable("block.minecraft.bed.no_sleep"), true);
-            ci.cancel();
+            cir.setReturnValue(false);
         }
     }
 }
